07bdfd07ecd4b1bfdda3adc95061ff1c1d822dfe
marcoputon <marcoputon@gmail.com>
2017-08-17 21:01:28 -0300

    Adicionando vértices e arestas, e rodando kruskal

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -1,55 +1,22 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/kruskal_min_spanning_tree.hpp>
+#include "Graph.h"
 #include <iostream>
-#include <map>
-
-using namespace boost;
 
 
-// Vértice é um array de tamanho 3
-typedef std::array<int, 3> Vertex;
+void G::add_vertex(Vertex v){
+    V vd = boost::add_vertex(g);
+    Vertex_map[v] = vd;
+    std::cout << vd << " - ";
+};
 
-// Grafo é uma lista de adjacencia com lista de vértices, arestas, não dirigido e com peso nas arestas
-typedef adjacency_list <vecS, vecS, undirectedS, no_property, property <edge_weight_t, int>> Graph;
-
-// Bagulhos pra iterar nas arestas e vértices
-typedef graph_traits <Graph>::edge_descriptor E;
-typedef graph_traits <Graph>::vertex_descriptor V;
-
-
-struct G {
-    Graph g;
-    std::map<Vertex, V> Vertex_map;
-
-    //  Adiciona o vértice no mapa de vértices tendo como chave o vertex_descriptor
-    void add_vertex(Vertex v){
-        V vd = boost::add_vertex(g);
-        Vertex_map[v] = vd;
-    };
-
-    //  Acho que vai bugar se adicionar aresta com vertices não contidos no Grafo
-    //  porque não vai adicionar no Vertex_map os novos vértices.
-    void add_edge(Vertex u, Vertex v, int w){
-        boost::add_edge(Vertex_map[u], Vertex_map[v], w, g);
-    };
+void G::add_edge(Vertex u, Vertex v, int w){
+    boost::add_edge(Vertex_map[u], Vertex_map[v], w, g);
 };
 
 
-
-
-int main() {
-    G graph;
-    Vertex u1 = {1, 2, 1};
-    Vertex u2 = {3, 5, 4};
-    Vertex u3 = {6, 6, 6};
-
-    graph.add_vertex(u1);
-    graph.add_vertex(u2);
-    graph.add_vertex(u3);
-
-    graph.add_edge(u1, u2, 3);
-    graph.add_edge(u1, u3, 2);
-    graph.add_edge(u2, u3, 1);
-
-    return 0;
+void print_edges(std::vector <E> spanning_tree, G graph){
+    for (std::vector <E>::iterator ei = spanning_tree.begin(); ei != spanning_tree.end(); ++ei) {
+        std::cout << source(*ei, graph.g) << " <--> " << target(*ei, graph.g)
+                  << " with weight of " << get(edge_weight, graph.g, *ei)
+                  << std::endl;
+    }
 }
